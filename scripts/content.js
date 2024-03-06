13943da09a0e5ad84d2f2343e57f9fd16314cacd
RafaelSaesPalaria <rafaelsaespalaria@gmail.com>
2024-03-06 10:27:01 -0300

    content.js function organized

--- scripts/content.js
+++ scripts/content.js
@@ -2,21 +2,33 @@
 
 chrome.runtime.onMessage.addListener(function name(message) {
     if (message.todo == "downloadCanvas") {
-        let canvas = document.querySelectorAll("canvas")[message.id]
-        let link = document.createElement("a")
-        let data = canvas.toDataURL()
-        link.download = data
-        link.href = data
-        link.click()
+        downloadCanvas(message.id)
     } else if (message.todo == "miniatureCanvas-All") {
-        let canvasd = document.querySelectorAll("canvas")
-        let canvas = {}
-        for (let i=0; i<canvasd.length;i++) {
-            canvas[i] = canvasd[i].toDataURL()
-        }
-        chrome.runtime.sendMessage({canvas,"todo":"create"})
+        miniatureCanvas_All()
     } else if (message.todo == "miniatureCanvas") {
-        let canvas = document.querySelectorAll("canvas")[message.id].toDataURL()
-        chrome.runtime.sendMessage({canvas,"todo":"edit","id":message.id})
+        miniatureCanvas(message.id)
     }
 })
+
+function downloadCanvas(id) {
+    let canvas = document.querySelectorAll("canvas")[id]
+    let link = document.createElement("a")
+    let data = canvas.toDataURL()
+    link.download = data
+    link.href = data
+    link.click()
+}
+
+function miniatureCanvas(id) {
+    let canvas = document.querySelectorAll("canvas")[id].toDataURL()
+    chrome.runtime.sendMessage({canvas,"todo":"edit","id":id})
+}
+
+function miniatureCanvas_All() {
+    let canvasd = document.querySelectorAll("canvas")
+    let canvas = {}
+    for (let i=0; i<canvasd.length;i++) {
+        canvas[i] = canvasd[i].toDataURL()
+    }
+    chrome.runtime.sendMessage({canvas,"todo":"create"})
+}
