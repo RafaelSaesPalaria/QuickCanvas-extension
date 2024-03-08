115146d974db97989e450d4675f35a1a99ab7069
RafaelSaesPalaria <rafaelsaespalaria@gmail.com>
2024-03-08 09:49:17 -0300

    options js components connected

---
+++ scripts/options.js
@@ -0,0 +1,21 @@
+var components = {
+    submit: document.querySelector('input[type="submit"]#submit'),
+    reset: document.querySelector('input[type=reset]#reset'),
+    preview: {
+        size: document.querySelector('input[type="range"]#preview-size'),
+        onecanvas: document.querySelector('input[type="checkbox"]#preview-onecanvas'),
+        landscape: document.querySelector('input[type="radio"]#preview-orientation-landscape'),
+        portrait: document.querySelector('input[type="radio"]#preview-orientation-portrait'),
+        transparent: document.querySelector('input[type="checkbox"]#preview-background-transparent'),
+        color: document.querySelector('input[type="color"]#preview-background-color'),
+    },
+    update: {
+        update: document.querySelector('input[type="checkbox"]#update'),
+        always: document.querySelector('input[type="radio"]#update-always'),
+        hovered: document.querySelector('input[type="radio"]#update-hovered'),
+        input: document.querySelector('input[type="checkbox"]#update-keep'),
+        interval: document.querySelector('input[type="number"]#update-interval')
+    }
+}
+
+console.log(components)
